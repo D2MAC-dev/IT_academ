44b5ee63bf35ac2f7a1e523522b5169ca44ed70d
d_bratchykov <D2MACjob@gmail.com>
2020-09-11 17:51:04 +0300

    fix all2

--- main2.py
+++ main2.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 xy = 2 + 2
 print(xy)
 #изменения
 print("Слияние страниц")   
-=======
 x = 2 + 2
 print(x)
-print("Слияние страниц")   
-# merget
-# merge 223
-print("начинай по новой")
->>>>>>> new_branch
