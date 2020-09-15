14d62c6e1cc7734e9548cc0116fd291bea782395
d_bratchykov <D2MACjob@gmail.com>
2020-09-15 22:55:52 +0300

    index mass of body

--- HomeWork/lesson_3.py
+++ HomeWork/lesson_3.py
@@ -2,8 +2,7 @@ m = int(input())
 h = int(input())
 x = h / 100
 imt = round(m / (x * x))
-# 50 - 20 = 30 steps
 first = round(int(imt))
-print("Ваш индекс массы тела :" + "" + str(imt))
 scale = '10' + "=" * first + str("|") + "="*(50 - imt) + "50"
+print("Ваш индекс массы тела :" + "" + str(imt))
 print(scale)
