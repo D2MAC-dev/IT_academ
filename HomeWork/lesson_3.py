47d606b7259cf33d810e0fc13422726ba33bc904
d_bratchykov <D2MACjob@gmail.com>
2020-09-20 18:55:27 +0300

    almost done

--- HomeWork/lesson_3.py
+++
@@ -1,6 +0,0 @@
-m = int(input("Введите Ваш вес (кг):"))
-h = int(input("Введите Ваш рост (см) :"))
-imt = round(int(m / (h / 100) ** 2))
-scale = '10' + "=" * imt + str("|") + "="*(50 - imt) + "50"
-print("Ваш индекс массы тела :" + "" + str(imt))
-print(scale)
