aa4bf94fe019db8e05cd9190778031ec0d1162ad
d_bratchykov <D2MACjob@gmail.com>
2020-10-21 16:10:34 +0300

    заполнил таблицу

--- Django/src/book_tabl/apps.py
+++ Django/src/book_tabl/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class BookTablConfig(AppConfig):
     name = 'book_tabl'
+    verbose_name = 'Справочник книг'
--- Django/src/book_tabl/models.py
+++ Django/src/book_tabl/models.py
@@ -6,7 +6,13 @@ class Book(models.Model):
     book_author = models.CharField('Автор', max_length = 200)
     book_publisher = models.CharField('Издательство', max_length = 200)
 
+    def __str__(self):
+        return self.book_name
+
 class Comment(models.Model):
     book = models.ForeignKey(Book, on_delete = models.CASCADE)
     user_name = models.CharField('Имя пользователя', max_length = 55)
     comment_text = models.CharField('текст комментари', max_length = 300)
+
+    def __str__(self):
+        return self.user_name
